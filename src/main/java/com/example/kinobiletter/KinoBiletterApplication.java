be4f4d99f1e8a260ba8e9470bc249d3c231eda37
Anja Trbos <antrb3968@oslomet.no>
2024-02-07 16:14:29 +0100

    setup

---
+++ src/main/java/com/example/kinobiletter/KinoBiletterApplication.java
@@ -0,0 +1,13 @@
+package com.example.kinobiletter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinoBiletterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KinoBiletterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kinobiletter/KinoBiletterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kinobiletter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinoBiletterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
